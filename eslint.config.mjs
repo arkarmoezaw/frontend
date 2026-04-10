d373fe0ea8c555ca26ac8ea47aadb66001c8f556
rkarmoezaw <rkarmoezaw.dev@gmail.com>
2026-04-10 22:57:46 +0630

    feat: initial commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,22 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettier from "eslint-config-prettier";
+import tailwind from "eslint-plugin-tailwindcss";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
-  ]),
+  ...tailwind.configs["flat/recommended"],
+  prettier,
+
+  {
+    rules: {
+      "react/prop-types": "off",
+      "tailwindcss/no-custom-classname": "off",
+    },
+  },
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
 ]);
 
 export default eslintConfig;
